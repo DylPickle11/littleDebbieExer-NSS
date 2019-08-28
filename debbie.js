a63a2fe6d1998d145f7bdb8260ff320c12b6d632
Dylan <Dylan.Griff11@gmail.com>
2019-08-28 10:31:53 -0500

    first commit

---
+++ debbie.js
@@ -0,0 +1,36 @@
+/*Lightening Exercise little Debbies */
+
+const debbieCake = {
+  name: "Strawberry Fields Forever",
+  filling:" Strawberry, Carefree Berries", 
+  amount: 6,
+  season: 'Christmas',
+  type: 'cake',
+  color:' pink'
+}
+
+const debbieCake2 = {
+  name: 'Crooked Cookie',
+  filling: 'Criminal Delight', 
+  amount: 10,
+  season: 'Bank Robbing Season',
+  type: 'cookie',
+  color: 'green'
+}
+
+const debbieCake3 = {
+  name: 'Secret Brownies',
+  filling: 'Secret Batter', 
+  amount: 5,
+  season: 'Mud season',
+  type:'Brownie',
+  color:' Brown'
+}
+
+
+let debbieCakeCollections = [];
+
+
+debbieCakeCollections.push(debbieCake, debbieCake2, debbieCake3)
+
+console.log(debbieCakeCollections)
